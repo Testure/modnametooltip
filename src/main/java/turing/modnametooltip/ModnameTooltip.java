7ec126fca36b74dbfb33cc627ce0ea205e44db58
Turing6 <39919441+Testure@users.noreply.github.com>
2024-03-31 21:38:51 -0500

    Better mod lookup

--- src/main/java/turing/modnametooltip/ModnameTooltip.java
+++ src/main/java/turing/modnametooltip/ModnameTooltip.java
@@ -6,17 +6,31 @@ import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.core.item.ItemStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.GameStartEntrypoint;
 
 import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
 
-public class ModnameTooltip implements ModInitializer {
+public class ModnameTooltip implements ModInitializer, GameStartEntrypoint {
     public static final String MOD_ID = "modnametooltip";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	private static final Map<String, ModContainer> MOD_ID_MAP = new HashMap<>();
 
     @Override
     public void onInitialize() {
         LOGGER.info("ModnameTooltip initialized.");
     }
+
+	@Override
+	public void beforeGameStart() {
+		for (ModContainer mod : FabricLoader.getInstance().getAllMods()) {
+			MOD_ID_MAP.put(mod.getMetadata().getId(), mod);
+		}
+	}
+
+	@Override
+	public void afterGameStart() {}
 
 	@Nullable
 	public static ModContainer getModForItem(ItemStack stack) {
@@ -25,13 +39,7 @@ public class ModnameTooltip implements ModInitializer {
 		name = name.substring(5);
 		if (name.contains(".")) name = name.substring(0, name.indexOf('.'));
 
-		for (ModContainer mod : FabricLoader.getInstance().getAllMods()) {
-			if (mod.getMetadata().getId().equals(name)) {
-				return mod;
-			}
-		}
-
-		return null;
+		return MOD_ID_MAP.get(name);
 	}
 
 	public static String getModnameForItem(ItemStack stack) {
