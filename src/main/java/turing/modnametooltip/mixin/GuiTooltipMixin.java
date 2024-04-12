872722d1b40d645aebec57f9063caa7271505bc7
LukeisStuff <lukeisawesome03@gmail.com>
2024-04-12 09:18:01 -0400

    Updated to 7.1

--- src/main/java/turing/modnametooltip/mixin/GuiTooltipMixin.java
+++ src/main/java/turing/modnametooltip/mixin/GuiTooltipMixin.java
@@ -1,6 +1,7 @@
 package turing.modnametooltip.mixin;
 
 import net.minecraft.client.gui.GuiTooltip;
+import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.lang.I18n;
 import net.minecraft.core.net.command.TextFormatting;
@@ -16,7 +17,7 @@ import turing.modnametooltip.ModnameTooltip;
 public class GuiTooltipMixin {
 	@Inject(method = "getTooltipText(Lnet/minecraft/core/item/ItemStack;ZLnet/minecraft/core/player/inventory/slot/Slot;)Ljava/lang/String;", at = @At(value = "TAIL", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
 	public void injectTooltip(ItemStack itemStack, boolean showDescription, Slot slot, CallbackInfoReturnable<String> cir, I18n trans, StringBuilder text) {
-		boolean discovered = slot == null || slot.discovered;
+		boolean discovered = slot == null || slot.getIsDiscovered(null);
 		if (discovered || ModnameTooltip.ignoreDiscovered) {
 			String modName = ModnameTooltip.useID ? ModnameTooltip.getModIdForItem(itemStack) : ModnameTooltip.getModnameForItem(itemStack);
 			modName = TextFormatting.formatted(modName, ModnameTooltip.FORMATS);
