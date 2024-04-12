ffe498998d5c9f7862a8267ba51106d6d9375bbb
Turing6 <39919441+Testure@users.noreply.github.com>
2024-04-12 13:53:15 -0500

    Fix crash from getIsDiscovered

--- src/main/java/turing/modnametooltip/mixin/GuiTooltipMixin.java
+++ src/main/java/turing/modnametooltip/mixin/GuiTooltipMixin.java
@@ -1,12 +1,13 @@
 package turing.modnametooltip.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiTooltip;
-import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.ItemStack;
 import net.minecraft.core.lang.I18n;
 import net.minecraft.core.net.command.TextFormatting;
 import net.minecraft.core.player.inventory.slot.Slot;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -15,9 +16,12 @@ import turing.modnametooltip.ModnameTooltip;
 
 @Mixin(value = GuiTooltip.class, remap = false)
 public class GuiTooltipMixin {
+	@Shadow
+	Minecraft mc;
+
 	@Inject(method = "getTooltipText(Lnet/minecraft/core/item/ItemStack;ZLnet/minecraft/core/player/inventory/slot/Slot;)Ljava/lang/String;", at = @At(value = "TAIL", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
 	public void injectTooltip(ItemStack itemStack, boolean showDescription, Slot slot, CallbackInfoReturnable<String> cir, I18n trans, StringBuilder text) {
-		boolean discovered = slot == null || slot.getIsDiscovered(null);
+		boolean discovered = slot == null || slot.getIsDiscovered(mc.thePlayer);
 		if (discovered || ModnameTooltip.ignoreDiscovered) {
 			String modName = ModnameTooltip.useID ? ModnameTooltip.getModIdForItem(itemStack) : ModnameTooltip.getModnameForItem(itemStack);
 			modName = TextFormatting.formatted(modName, ModnameTooltip.FORMATS);
